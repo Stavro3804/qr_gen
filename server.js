7175fbbca7bc94573a841eb8d4f2455428aae107
rajat3804 <rajatverma3804@gmail.com>
2025-03-20 20:07:48 +0530

    rebuilding

--- server.js
+++ server.js
@@ -20,4 +20,4 @@ app.get("/generate", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
