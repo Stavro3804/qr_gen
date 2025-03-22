1e75ab0e366bfdd8f720f605631f34fdc659b929
rajat3804 <rajatverma3804@gmail.com>
2025-03-22 11:02:11 +0530

    Fixing

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ app.get("/generate", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
